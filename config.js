b9945043fbe746bf15f81eeec36a0c532f389ea1
Mike <mike.bryant.jr@icloud.com>
2018-12-12 22:56:15 -0700

    css and components

--- config.js
+++ config.js
@@ -3,3 +3,11 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhos
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
+
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/2fc4afe9-9702-4b70-97b3-eb2c96b9ed53/token";
+const instanceLocator = "v1:us1:2fc4afe9-9702-4b70-97b3-eb2c96b9ed53";
+
+export { tokenUrl, instanceLocator }
+
+// security key ChatKit f4e67ae4-25ae-4dcb-9f27-803df066ea08:5AajcotVkhQiUWF1/taPZFiGN4xE+laC7iZgMP33IDU=
