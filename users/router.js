ff52c7923baf266b76b53848b1c7ec84b84098d3
Mike Bryant <mike.bryant.jr@icloud.com>
2018-12-17 16:31:44 -0700

    chatkit

--- users/router.js
+++ users/router.js
@@ -12,6 +12,8 @@ router.post('/', jsonParser, (req, res) => {
 	const requiredFields = ['username', 'password'];
 	const missingField = requiredFields.find(field => !(field in req.body));
 
+
+
 	if (missingField) {
 		return res.status(422).json({
 			code: 422,
@@ -124,16 +126,12 @@ router.post('/', jsonParser, (req, res) => {
 			}
 			res.status(500).json({ code: 500, message: 'Internal server error' });
 		});
-});
+	;
 
-// Never expose all your users like below in a prod application
-// we're just doing this so we have a quick way to see
-// if we're creating users. keep in mind, you can also
-// verify this in the Mongo shell.
-router.get('/', (req, res) => {
-	return User.find()
-		.then(users => res.json(users.map(user => user.serialize())))
-		.catch(err => res.status(500).json({ message: 'Internal server error' }));
-});
+	router.get('/', (req, res) => {
+		return User.find()
+			.then(users => res.json(users.map(user => user.serialize())))
+			.catch(err => res.status(500).json({ message: 'Internal server error' }));
+	});
 
-module.exports = { router };
+	module.exports = { router };
