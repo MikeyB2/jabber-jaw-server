818338c36d4b84ad44d4a8562b71ef5b7f1bf722
Mike <mike.bryant.jr@icloud.com>
2018-12-20 22:44:56 -0700

    travis correct

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jabber-jaw';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-jabber-jaw';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret_string';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
 exports.tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/2fc4afe9-9702-4b70-97b3-eb2c96b9ed53/token";
